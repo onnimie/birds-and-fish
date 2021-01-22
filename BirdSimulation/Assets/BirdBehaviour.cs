9b00b14ae7d868bc179eb6a5c5c3627bb84a7d61
Onni Miettinen <onni.miettinen@aalto.fi>
2021-01-22 19:00:52 +0200

    movement, boundaries, constant-holder-objects

--- BirdSimulation/Assets/BirdBehaviour.cs
+++ BirdSimulation/Assets/BirdBehaviour.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BirdBehaviour : MonoBehaviour
 {
+    public BirdValues birdValues;
+    float xBound = 60f;
+    float yBound = 24f;
+
     // Start is called before the first frame update
     void Start()
     {
+        birdValues = GameObject.Find("BirdGod").GetComponent<BirdValues>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, birdValues.baseSpeed, 0);
+
+        ReactOnOutOfBounds();
+    }
+
+    private void ReactOnOutOfBounds()
+    {
+        if (transform.position.x < -xBound || transform.position.x > xBound)
+        {
+            transform.position = new Vector3(-1 * transform.position.x, transform.position.y, 0);
+        }
+        if (transform.position.y < -yBound || transform.position.y > yBound)
+        {
+            transform.position = new Vector3(transform.position.x, -1 * transform.position.y, 0);
+        }
     }
 }
---
+++ BirdSimulation/Assets/FishValues.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FishValues: MonoBehaviour
+{
+    public float baseSpeed = 0.1f;
+
+}
